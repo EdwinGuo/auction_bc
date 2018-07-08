7b6673ceb53cb2dc9fa4b7df9dd188d89cd0e8c7
Edwinguo <edwinguo@Edwinguos-MacBook-Pro.local>
2018-07-08 14:27:23 -0400

    Update the test for the failure case

--- test/01testauction.js
+++ test/01testauction.js
@@ -1,6 +1,11 @@
 var Auction = artifacts.require("Auction");
-expect = require("chai").expect;
-//expect = require("web3").expect;
+chai = require("chai");
+
+chaiAsPromised = require("chai-as-promised")
+
+chai.use(chaiAsPromised);
+expect = chai.expect;
+
 var contractAddress = "0x627306090abab3a6e1400e9345bc60c78a8bef57";
 var weiUnit = 1000000000000000000;
 
@@ -65,4 +70,31 @@ contract("Test Auction contract", function(accounts){
 
 
     });
+
+
+    describe("check contract Value when bid with lower value", function(){
+        it("check the current auction price when bid value is lower than the current value", function(){
+            // make a bit that lower then the latestBid
+            var bidTxObject = {
+                from: accounts[4],
+                value: web3.toWei('2', 'ether')
+            };
+
+            return expect(auctionContract.bid(bidTxObject)).to.be.eventually.rejected;
+
+            console.log("Submitted the new lower bid");
+
+        });
+
+        it("check the current auction price", function(){
+            return auctionContract.latestBid().then(function(res){
+                console.log("latestBid: " + res);
+                expect(res / weiUnit).to.be.equal(3);
+            });
+
+        });
+
+    });
+
+
 })
